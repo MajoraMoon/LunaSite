24e9d35cde8511aa5fa3823f2c88a40b856983cb
majora <maxwell.kellwood@tuta.io>
2025-01-16 17:16:56 +0100

    particle effects added with tsparticles

---
+++ scripts/lib/particleSetup.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const particleId = "tsparticles";
+
+  if (!document.getElementById(particleId)) {
+    console.warn(`No <div id='${particleId}'> found. Check your html.`);
+    return;
+  }
+
+  // tsParticles mit externer JSON-Config laden
+  tsParticles
+    .load({
+      id: particleId,
+      url: "../scripts/lib/particles-config.json",
+    })
+    .then((container) => {
+      console.log("✅ tsParticles (JSON-Config) erfolgreich geladen!");
+    })
+    .catch((error) => {
+      console.error("❌ Fehler beim Laden der tsParticles-Config:", error);
+    });
+});
