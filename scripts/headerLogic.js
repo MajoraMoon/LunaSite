6ad461af7772ef6760fe9c46ea6d546cba0b2a4b
majora <maxwell.kellwood@tuta.io>
2025-01-17 17:47:01 +0100

    little improvement im slow but whatever

--- scripts/headerLogic.js
+++ scripts/headerLogic.js
@@ -1,9 +1,16 @@
 function initializeHeaderLogic() {
-  const currentPath = window.location.pathname.split("/").pop();
+  const currentPath = window.location.pathname;
+
   const navLinks = document.querySelectorAll(".nav-link");
 
   navLinks.forEach((link) => {
-    if (link.href.endsWith(currentPath)) {
+    console.log(currentPath);
+
+    if (currentPath.includes("projects") && link.href.includes("projects")) {
+      link.classList.add("active");
+    } else if (currentPath.includes("index") && link.href.includes("index")) {
+      link.classList.add("active");
+    } else if (currentPath.includes("about") && link.href.includes("about")) {
       link.classList.add("active");
     } else {
       link.classList.remove("active");
